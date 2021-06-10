3426e2f224e516a04faeb2e694eb828112f3d262
Tomas Dostal <tomas@coyote.co.uk>
2021-06-10 23:04:05 +0100

    Loads properties from API

--- src/components/PropertyListing/tests/PropertyListing.spec.js
+++ src/components/PropertyListing/tests/PropertyListing.spec.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import PropertyListing from '../PropertyListing';
+
+jest.mock("../../../modules/api/connectors", () => {
+    const DUMMY_PROPERTY = {
+        id: 73864112,
+        bedrooms: 3,
+        summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+        displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+        propertyType: 'Flat',
+        price: 1950000,
+        branchName: 'M2 Property, London',
+        propertyUrl: '/property-for-sale/property-73864112.html',
+        contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+        propertyTitle: '3 bedroom flat for sale',
+        mainImage: 'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg'
+    };
+
+    return {
+        getProperties: jest.fn().mockImplementation(() => {
+            return Promise.resolve(Array(5).fill(DUMMY_PROPERTY));
+        }),
+    };
+});
 
 describe('PropertyListing', () => {
 
@@ -9,8 +32,12 @@ describe('PropertyListing', () => {
         expect(wrapper.find('.PropertyListing')).toHaveLength(1);
     });
 
-    it('should render five property cards', () => {
-        const wrapper = shallow(<PropertyListing/>);
+    it('should render five property cards', async () => {    
+        const wrapper = mount(<PropertyListing/>);         
+        await act(async () => {
+            await new Promise(resolve => setTimeout(resolve, 0));
+            wrapper.update();
+        });
         expect(wrapper.find('PropertyCard')).toHaveLength(5);
     });
 });
